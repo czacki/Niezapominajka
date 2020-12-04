b7e3791658fc826e609947e42e64e0ba3917eaca
J3ykob <jaqbmaliszewski@gmail.com>
2020-12-04 14:59:02 +0100

    second

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,37 +3,73 @@ $(document).ready(function () {
 	$('.festiwal-cover').height(document.querySelector('.festiwal').offsetHeight)
 	$('.slides').width(document.querySelector('.inside').offsetWidth)
 	$('.slides').height(document.querySelector('.inside').offsetHeight)
+
+	$('.slides').addClass('slide-hide')
+
 	console.log('ready!')
-	let video = document.querySelector('video')
+
+	let video = document.querySelector('.ft-video')
+
+	//load imges
 
 	var buffered = true
 
 	setInterval(() => {
 		// console.log(video.buffered)
 		if (video.buffered) {
-			console.log('loaded')
 			$('.loading').addClass('stop-loading')
 			// buffered = true
-			video.play()
+			// video.play()
 		}
-	}, 1000)
+	}, 3000)
 
 	var displaySlides = function () {}
+})
 
-	setInterval(() => {
-		console.log('buffered')
-		if (buffered == true) {
-			$('.festiwal').each((i) => {
-				let e = $('.festiwal').eq(i).find('.slides')
+$('.festiwal').click(function () {
+	$(this).data('expanded') == true
+	$(this).data('expanded', !$(this).data('expanded'))
 
-				if (e.find('.slide.slide-left').length == 0 && e.find('[class=slide]').length == 0) {
-					e.find('.slide-right').removeClass('slide-right').addClass('slide-left')
-					e.find('[class=slide]').first().addClass('slide-right')
+	var interval
+
+	if ($(this).data('expanded') == true) {
+		// $(this).css({ height: '50vh' })
+		$(this).children().eq(1).velocity({ height: '50vh' }, 'slow')
+		$(this).children().eq(0).velocity({ height: '50vh' }, 'slow')
+		$(this).children().eq(0).children().eq(0).css({ transform: 'translate(-600px)' })
+		$(this).children().eq(0).children().eq(1).css({ transform: 'translate(600px)' })
+
+		$(this).find('.slides').removeClass('slide-hide')
+
+		$(this).find('.slides').css({ height: '50vh' })
+
+		$(this).find('.ft-video').trigger('play')
+
+		setTimeout(() => {
+			interval = setInterval(() => {
+				if ($(this).find('.slide.slide-left').length == 0 && $(this).find('[class=slide]').length == 0) {
+					$(this).find('.slide-right').removeClass('slide-right').addClass('slide-left')
+					$(this).find('[class=slide]').first().addClass('slide-right')
 				} else {
-					e.find('[class=slide]').first().addClass('slide-right')
-					e.find('.slide-left').first().removeClass('slide-left')
+					$(this).find('[class=slide]').first().addClass('slide-right')
+					$(this).find('.slide-left').first().removeClass('slide-left')
 				}
-			})
-		}
-	}, 2000)
+			}, 8000)
+		}, 1000)
+	} else {
+		clearInterval(interval)
+		$(this).css({ height: '10vh' })
+		$(this).children().eq(1).velocity({ height: '10vh' }, 'slow')
+		$(this).children().eq(0).velocity({ height: '10vh' }, 'slow')
+		$(this).children().eq(0).children().eq(0).css({ transform: 'translate(0)' })
+		$(this).children().eq(0).children().eq(1).css({ transform: 'translate(0)' })
+
+		$(this).find('.slides').addClass('slide-hide')
+
+		$(this).find('.slides').css({ height: '10vh' })
+
+		$(this).find('.slide').removeClass('slide-right').addClass('slide-left')
+
+		$(this).find('.ft-video').trigger('pause')
+	}
 })
